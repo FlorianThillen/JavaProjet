04c6faeb4485e29e4bcc3a85409e1f9a0904d49a
canindo luke <caniluke@gmail.com>
2025-04-16 15:23:16 +0200

    Progress user task "add new repair"

--- src/ViewPackage/Job/RepairPanel.java
+++ src/ViewPackage/Job/RepairPanel.java
@@ -2,11 +2,11 @@ package ViewPackage.Job;
 
 import javax.swing.*;
 import java.awt.*;
-import java.lang.reflect.Array;
+//import java.lang.reflect.Array;
 import java.util.Vector;
 
 import ControllerPackage.Controller;
-import jdk.incubator.vector.VectorOperators;
+//import jdk.incubator.vector.VectorOperators;
 
 public class RepairPanel extends JPanel {
     private Controller controller;
@@ -17,24 +17,69 @@ public class RepairPanel extends JPanel {
         this.controller = controller;
 
         JPanel titlePanel = new JPanel();
-        JPanel contentPanel = new JPanel();
+        JPanel contentPanel = new ContentPanel();
+        JPanel confirmationPanel = new JPanel();
 
         titlePanel.add(new JLabel("Title"));
-        JComboBox combox = new JComboBox<>(query_localities());
-        combox.setMaximumRowCount(6);
-        contentPanel.add(combox);
+
+        confirmationPanel.setLayout(new BoxLayout(confirmationPanel, BoxLayout.X_AXIS));
+        confirmationPanel.add(new JButton("Return"));
+        confirmationPanel.add(new JButton("Confirm"));
 
         add(titlePanel);
         add(contentPanel);
+        add(confirmationPanel);
     }
 
-    Vector<String> query_localities() {
-        Vector<String> localities = new Vector<String>();
-        for(int i_choice = 0; i_choice < 20; i_choice++) {
-            localities.add(String.format("Test%d", i_choice));
+    // Content Panel sub class
+    private class ContentPanel extends JPanel {
+
+        private JComboBox<String> localitiesCombox = new JComboBox<String>();
+        private JComboBox<String> StationsCombox = new JComboBox<String>();
+        private JComboBox<String> BikesCombox = new JComboBox<String>();
+        private JComboBox<String> MecasCombox = new JComboBox<String>();
+        private JComboBox<String> StatesCombox = new JComboBox<String>();
+
+        public ContentPanel() {
+            add(localitiesCombox);
+            add(StationsCombox);
+            add(BikesCombox);
+            add(MecasCombox);
+            add(StatesCombox);
         }
-        return localities;
+
+        JComboBox<String> create_locality_choices() {
+            JComboBox<String> combox = new JComboBox<>(query_localities());
+            combox.setMaximumRowCount(6);
+            return  combox;
+        }
+
+        Vector<String> query_localities() {
+            Vector<String> localities = new Vector<String>();
+            for(int i_choice = 0; i_choice < 20; i_choice++) {
+                localities.add(String.format("Test%d", i_choice));
+            }
+            return localities;
+        }
+
+        Vector<String> query_stations() {
+            return new Vector<>();
+        }
+
+        Vector<String> query_bikes() {
+            return new Vector<>();
+        }
+
+        Vector<String> query_mechanics() {
+            return new Vector<>();
+        }
+
+        Vector<String> query_repair_states() {
+            return new Vector<>();
+        }
+
+        Vector<String> evaluate_price() {
+            return new Vector<>();
+        }
     }
-
-
 }
