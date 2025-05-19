ac8b9314cb8ff505c279f8e6cc58a6553d3e030a
caniluke <caniluke@gmail.com>
2025-05-19 13:04:10 +0200

    Added BrandDAO class, not working

---
+++ src/DataAccesPackage/BrandDAO.java
@@ -0,0 +1,27 @@
+package DataAccesPackage;
+
+import ExceptionsPackage.DataAccesException;
+import ModelsPackage.BrandModel;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class BrandDAO {
+    ArrayList<BrandModel> fetchAllBrands() {
+        ArrayList<BrandModel> brandModels = new ArrayList<BrandModel>();
+
+        Connection connection = SingletonConnection.getInstance();
+
+        String query = "";
+
+        try (PreparedStatement stmt = connection.prepareStatement(query)) {
+
+        } catch (SQLException e) {
+            throw new DataAccesException("Erreur de récupération des coûts moyen de réparation", e);
+            throw new DataAccesException("Erreur de récupération des marques", e);
+        }
+        return brandModels;
+    }
+}
