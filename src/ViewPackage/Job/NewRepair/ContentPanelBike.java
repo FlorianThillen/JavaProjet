059a7ad4ec2ece43a4d4d69b7636aef39b1e10d4
caniluke <caniluke@gmail.com>
2025-05-21 23:19:30 +0200

    TM new repair progress

--- src/ViewPackage/Job/NewRepair/ContentPanelBike.java
+++ src/ViewPackage/Job/NewRepair/ContentPanelBike.java
@@ -2,16 +2,14 @@ package ViewPackage.Job.NewRepair;
 
 import ExceptionsPackage.DataAccesException;
 import ModelsPackage.BikeModel;
-import ModelsPackage.LocalityModel;
 import ModelsPackage.StationModel;
 
 import javax.swing.*;
 import java.util.ArrayList;
-import java.util.Stack;
 
 public class ContentPanelBike extends ContentPanelState {
     public ContentPanelBike() throws DataAccesException {
-        setNextState(new ContentPanelMechanic());
+        setNextState(new ContentPanelRepair());
 
         ArrayList<BikeModel> bikes = controller.getBikesFromStation((StationModel) getPreviousState().getInputValue());
 
--- src/ViewPackage/Job/NewRepair/ContentPanelRepair.java
+++ src/ViewPackage/Job/NewRepair/ContentPanelRepair.java
@@ -1,15 +1,13 @@
 package ViewPackage.Job.NewRepair;
 
 import ExceptionsPackage.DataAccesException;
-import ModelsPackage.BikeModel;
 import ModelsPackage.MechanicModel;
-import ModelsPackage.StationModel;
 
 import javax.swing.*;
 import java.util.ArrayList;
 
-public class ContentPanelMechanic extends ContentPanelState {
-    public ContentPanelMechanic() throws DataAccesException {
+public class ContentPanelRepair extends ContentPanelState {
+    public ContentPanelRepair() throws DataAccesException {
         ArrayList<MechanicModel> Mechanics = controller.getAllMechanics();
 
         DefaultListModel<MechanicModel> listModel = new DefaultListModel<>();
